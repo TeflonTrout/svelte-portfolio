73d42d78088a5b7e3461ac40af45d3ac69c72a45
TeflonTrout <jt@joy.world>
2025-03-08 13:54:40 -0500

    Updating resume

--- src/lib/Experience.svelte
+++ src/lib/Experience.svelte
@@ -1,145 +1,279 @@
 <script lang="ts">
-    import WorkCard from "./components/WorkCard.svelte";
-    import PNC from "./images/pncLogo.png"
-    import IronGrid from "./images/ironGridLogo.png"
-    import Joyworld from "./images/joyworldLogo.jpg"
+  import WorkCard from "./components/WorkCard.svelte";
+  import PNC from "./images/pncLogo.png";
+  import IronGrid from "./images/ironGridLogo.png";
+  import KingPool from "./images/KingPool.png";
+  import Joyworld from "./images/joyworldLogo.jpg";
 
-    export let innerWidth:number 
-
+  export let innerWidth: number;
 </script>
 
 <div class="workPage">
-    <h1>Work Experience</h1>
-    <div class="container">
-            <WorkCard 
-                logo={IronGrid}
-                title="Software Engineer" 
-                company="Freelance/Iron Grid" 
-                date="May 2023 - Present"
-                techStack={
-                    innerWidth <= 786 
-                        ? ["Next.js", "|", "TypeScript", "|", "0Auth", "|", "Supabase", "|", "Postgres", "|", "API", "|", "CSS", "|", "Git/GitHub", "|", "CI/CD"].filter(x => x != "|")
-                        : ["Next.js", "|", "TypeScript", "|", "0Auth", "|", "Supabase", "|", "Postgres", "|", "API", "|", "CSS", "|", "Git/GitHub", "|", "CI/CD"]
-                }
-                description={[
-                    "Developed a comprehensive web application leveraging Next.js and TypeScript to create a responsive, user-friendly interface.",
-                    "Integrated OAuth-based authentication, enabling secure and seamless user login through various providers.",
-                    "Utilized Supabase and Postgres for backend development, focusing on efficient data storage, retrieval, and management.",
-                    "Worked extensively with APIs to enhance application functionality and improve user experience.",
-                    "Employed CSS for frontend styling, ensuring a visually appealing and intuitive UI.",
-                    "Managed codebase using Git/GitHub, adhering to best practices for version control.",
-                    "Implemented CI/CD pipelines for automated testing and deployment, enhancing application reliability and development workflow."            
-                ]}
-            />
-            <WorkCard 
-                logo={Joyworld}
-                title="Software Engineer"
-                company={innerWidth <= 786 ? "JOYWORLD" : "JOYWORLD, Inc"} 
-                date="June 2021 - May 2023"
-                techStack={
-                innerWidth <= 786 
-                    ? ["React", "|", "Next.js", "|", "TypeScript", "|", "Web3.js", "|", "GraphQL", "|", "MongoDB", "|", "API", "|", "Deno.js", "|", "AWS", "|", "CI/CD"].filter(x => x != "|")
-                    : ["React", "|", "Next.js", "|", "TypeScript", "|", "Web3.js", "|", "GraphQL", "|", "MongoDB", "|", "API", "|", "Deno.js", "|", "AWS", "|", "CI/CD"]
-                }
-                description={[
-                "Developed user-centric Web3-enabled marketplace with React and Next.js, focusing on performance and scalability.",
-                "Utilized TypeScript and Web3.js for type-safe code and secure Ethereum smart contract interactions.",
-                "Employed GraphQL and MongoDB for efficient data retrieval and robust storage, serving both web and NFT use-cases.",
-                "Integrated multiple APIs to enhance application functionality for both traditional web and blockchain platforms.",
-                "Managed backend using Deno.js and deployed on AWS for high performance and reliability.",
-                "Implemented CI/CD pipelines for automated testing and deployment, optimizing the development workflow."            
-                ]}
-            />
-            <WorkCard 
-                logo={PNC}
-                title="Lead Loan Analyst" 
-                company="PNC Bank" 
-                date="Dec. 2018 - June 2021"
-                techStack={
-                    innerWidth <= 786
-                        ? ["Excel", "|", "SQL", "|", "CRM Software", "|"].filter(x => x != "|")
-                        : ["Excel", "|", "SQL", "|", "CRM Software", "|", "Risk Assessment Software", "|", "Data Visualization Tools"]
-                }
-                description={[
-                    "Reviewed and analyzed loan applications, applying data-driven techniques to assess creditworthiness, similar to evaluating code quality and system reliability in engineering.",
-                    "Adapted workflows to comply with federal regulations, demonstrating adaptability to procedural changes, a skill transferable to agile software development environments.",
-                    "Utilized financial software tools for loan processing, gaining experience in specialized software systems akin to specialized libraries and frameworks in engineering.",
-                    "Collaborated cross-functionally with sales, underwriting, and compliance teams, honing communication and teamwork skills relevant in software development projects.",
-                    "Managed large datasets of financial information, showcasing data management skills transferable to database design and implementation in software engineering."            
-                ]}
-            />
-    </div>
+  <h1>Work Experience</h1>
+  <div class="container">
+    <WorkCard
+      logo={KingPool}
+      backgroundColor="#DA1830"
+      title="Lead Front End Engineer"
+      company="KingPool"
+      date="October 2024 - Present"
+      techStack={innerWidth <= 786
+        ? [
+            "Next.js",
+            "|",
+            "TypeScript",
+            "|",
+            "0Auth",
+            "|",
+            "Supabase",
+            "|",
+            "Postgres",
+            "|",
+            "API",
+            "|",
+            "CSS",
+            "|",
+            "Git/GitHub",
+            "|",
+            "CI/CD",
+          ].filter((x) => x != "|")
+        : [
+            "Next.js",
+            "|",
+            "TypeScript",
+            "|",
+            "0Auth",
+            "|",
+            "Supabase",
+            "|",
+            "Postgres",
+            "|",
+            "API",
+            "|",
+            "CSS",
+            "|",
+            "Git/GitHub",
+            "|",
+            "CI/CD",
+          ]}
+      description={[
+        "Developed a comprehensive web application leveraging Next.js and TypeScript to create a responsive, user-friendly interface.",
+        "Integrated OAuth-based authentication, enabling secure and seamless user login through various providers.",
+        "Utilized Supabase and Postgres for backend development, focusing on efficient data storage, retrieval, and management.",
+        "Worked extensively with APIs to enhance application functionality and improve user experience.",
+        "Employed CSS for frontend styling, ensuring a visually appealing and intuitive UI.",
+        "Managed codebase using Git/GitHub, adhering to best practices for version control.",
+        "Implemented CI/CD pipelines for automated testing and deployment, enhancing application reliability and development workflow.",
+      ]}
+    />
+    <WorkCard
+      logo={IronGrid}
+      title="Software Engineer"
+      company="Iron Grid"
+      date="May 2023 - October 2024"
+      techStack={innerWidth <= 786
+        ? [
+            "Next.js",
+            "|",
+            "TypeScript",
+            "|",
+            "0Auth",
+            "|",
+            "Supabase",
+            "|",
+            "Postgres",
+            "|",
+            "API",
+            "|",
+            "CSS",
+            "|",
+            "Git/GitHub",
+            "|",
+            "CI/CD",
+          ].filter((x) => x != "|")
+        : [
+            "Next.js",
+            "|",
+            "TypeScript",
+            "|",
+            "0Auth",
+            "|",
+            "Supabase",
+            "|",
+            "Postgres",
+            "|",
+            "API",
+            "|",
+            "CSS",
+            "|",
+            "Git/GitHub",
+            "|",
+            "CI/CD",
+          ]}
+      description={[
+        "Developed a comprehensive web application leveraging Next.js and TypeScript to create a responsive, user-friendly interface.",
+        "Integrated OAuth-based authentication, enabling secure and seamless user login through various providers.",
+        "Utilized Supabase and Postgres for backend development, focusing on efficient data storage, retrieval, and management.",
+        "Worked extensively with APIs to enhance application functionality and improve user experience.",
+        "Employed CSS for frontend styling, ensuring a visually appealing and intuitive UI.",
+        "Managed codebase using Git/GitHub, adhering to best practices for version control.",
+        "Implemented CI/CD pipelines for automated testing and deployment, enhancing application reliability and development workflow.",
+      ]}
+    />
+    <WorkCard
+      logo={Joyworld}
+      title="Software Engineer"
+      company={innerWidth <= 786 ? "JOYWORLD" : "JOYWORLD, Inc"}
+      date="June 2021 - May 2023"
+      techStack={innerWidth <= 786
+        ? [
+            "React",
+            "|",
+            "Next.js",
+            "|",
+            "TypeScript",
+            "|",
+            "Web3.js",
+            "|",
+            "GraphQL",
+            "|",
+            "MongoDB",
+            "|",
+            "API",
+            "|",
+            "Deno.js",
+            "|",
+            "AWS",
+            "|",
+            "CI/CD",
+          ].filter((x) => x != "|")
+        : [
+            "React",
+            "|",
+            "Next.js",
+            "|",
+            "TypeScript",
+            "|",
+            "Web3.js",
+            "|",
+            "GraphQL",
+            "|",
+            "MongoDB",
+            "|",
+            "API",
+            "|",
+            "Deno.js",
+            "|",
+            "AWS",
+            "|",
+            "CI/CD",
+          ]}
+      description={[
+        "Developed user-centric Web3-enabled marketplace with React and Next.js, focusing on performance and scalability.",
+        "Utilized TypeScript and Web3.js for type-safe code and secure Ethereum smart contract interactions.",
+        "Employed GraphQL and MongoDB for efficient data retrieval and robust storage, serving both web and NFT use-cases.",
+        "Integrated multiple APIs to enhance application functionality for both traditional web and blockchain platforms.",
+        "Managed backend using Deno.js and deployed on AWS for high performance and reliability.",
+        "Implemented CI/CD pipelines for automated testing and deployment, optimizing the development workflow.",
+      ]}
+    />
+    <WorkCard
+      logo={PNC}
+      title="Lead Loan Analyst"
+      company="PNC Bank"
+      date="Dec. 2018 - June 2021"
+      techStack={innerWidth <= 786
+        ? ["Excel", "|", "SQL", "|", "CRM Software", "|"].filter(
+            (x) => x != "|"
+          )
+        : [
+            "Excel",
+            "|",
+            "SQL",
+            "|",
+            "CRM Software",
+            "|",
+            "Risk Assessment Software",
+            "|",
+            "Data Visualization Tools",
+          ]}
+      description={[
+        "Reviewed and analyzed loan applications, applying data-driven techniques to assess creditworthiness, similar to evaluating code quality and system reliability in engineering.",
+        "Adapted workflows to comply with federal regulations, demonstrating adaptability to procedural changes, a skill transferable to agile software development environments.",
+        "Utilized financial software tools for loan processing, gaining experience in specialized software systems akin to specialized libraries and frameworks in engineering.",
+        "Collaborated cross-functionally with sales, underwriting, and compliance teams, honing communication and teamwork skills relevant in software development projects.",
+        "Managed large datasets of financial information, showcasing data management skills transferable to database design and implementation in software engineering.",
+      ]}
+    />
+  </div>
 </div>
 
+<style>
+  @keyframes slide-in {
+    to {
+      opacity: 1;
+      transform: translateX(0);
+    }
+  }
 
-<style> 
-    @keyframes slide-in {
-        to {
-            opacity: 1;
-            transform: translateX(0)
-        }
+  /* MOBILE */
+  @media screen and (max-width: 786px) {
+    .workPage {
+      display: flex;
+      flex-direction: column;
+      justify-content: flex-start;
+      align-items: center;
+      width: 100%;
     }
 
-    /* MOBILE */
-    @media screen and (max-width: 786px) {
-        .workPage {
-            display: flex;
-            flex-direction: column;
-            justify-content: flex-start;
-            align-items: center;
-            width: 100%;
-        }
-
-        .workPage h1 {
-            font-size: 48px;
-            text-decoration: underline;
-            animation: 1s slide-in ease-out .15s forwards;
-            transform: translateX(-110%);
-            opacity: 0;
-        }
-        
-        .workPage .container {
-            display: flex;
-            flex-direction: column;
-            width: 100%;
-            justify-content: center;
-            align-items: center;
-            animation: 1s slide-in ease-out .3s forwards;
-            /* transform: translateX(-110%); */
-            opacity: 0;
-
-        }
+    .workPage h1 {
+      font-size: 48px;
+      text-decoration: underline;
+      animation: 1s slide-in ease-out 0.15s forwards;
+      transform: translateX(-110%);
+      opacity: 0;
     }
-    
-    @media screen and (min-width: 786px) {
-        .workPage {
-            display: flex;
-            flex-direction: column;
-            justify-content: flex-start;
-            align-items: center;
-            width: 100%;
-            margin-top: 70px;
-            min-height: 1260px;
-        }
 
-        .workPage h1 {
-            font-size: 48px;
-            text-decoration: underline;
-            animation: 1s slide-in ease-out .15s forwards;
-            transform: translateX(-110%);
-            opacity: 0;
-        }
-        
-        .workPage .container {
-            display: flex;
-            flex-direction: column;
-            width: 100%;
-            justify-content: center;
-            align-items: center;
-            animation: 1s slide-in ease-out .3s forwards;
-            /* transform: translateX(-110%); */
-            opacity: 0;
-
-        }
+    .workPage .container {
+      display: flex;
+      flex-direction: column;
+      width: 100%;
+      justify-content: center;
+      align-items: center;
+      animation: 1s slide-in ease-out 0.3s forwards;
+      /* transform: translateX(-110%); */
+      opacity: 0;
     }
+  }
+
+  @media screen and (min-width: 786px) {
+    .workPage {
+      display: flex;
+      flex-direction: column;
+      justify-content: flex-start;
+      align-items: center;
+      width: 100%;
+      margin-top: 70px;
+      min-height: 1260px;
+    }
+
+    .workPage h1 {
+      font-size: 48px;
+      text-decoration: underline;
+      animation: 1s slide-in ease-out 0.15s forwards;
+      transform: translateX(-110%);
+      opacity: 0;
+    }
+
+    .workPage .container {
+      display: flex;
+      flex-direction: column;
+      width: 100%;
+      justify-content: center;
+      align-items: center;
+      animation: 1s slide-in ease-out 0.3s forwards;
+      /* transform: translateX(-110%); */
+      opacity: 0;
+    }
+  }
 </style>
--- src/lib/components/WorkCard.svelte
+++ src/lib/components/WorkCard.svelte
@@ -1,209 +1,218 @@
 <script lang="ts">
   import { onMount } from "svelte";
 
-    export let logo:string;
-    export let title:string;
-    export let company:string;
-    export let date:string;
-    export let techStack:Array<string>;
-    export let description:Array<string>;
-        let innerWidth:number = window.innerWidth
+  export let logo: string;
+  export let title: string;
+  export let company: string;
+  export let backgroundColor = "transparent";
+  export let date: string;
+  export let techStack: Array<string>;
+  export let description: Array<string>;
+  let innerWidth: number = window.innerWidth;
 
-    onMount(() => {
-        function onResize() {
-			innerWidth = window.innerWidth;
-		}
-		window.addEventListener('resize', onResize);
-		return () => window.removeEventListener('resize', onResize);    
-    })
+  onMount(() => {
+    function onResize() {
+      innerWidth = window.innerWidth;
+    }
+    window.addEventListener("resize", onResize);
+    return () => window.removeEventListener("resize", onResize);
+  });
 </script>
 
 <div class="workCardWrapper">
-        <div class="workCard">
-            <div class="heading">
-                <img src={logo} alt="company">
-                <h1 class="company">{company}&nbsp;</h1>
-                <h1 class="title">{innerWidth > 786 ? "-" : ""} {title}</h1>
-            </div>
-            <h4 class="date">{date}</h4>
-                <span></span>
-                    <div class="techStack">
-                        {#each techStack as tech}
-                            <p>{tech}</p>
-                        {/each}
-                    </div>
-            <span></span>
-            <div class="description">
-                <ul>
-                    {#each description as item}
-                        <li>{item}</li>
-                    {/each}
-                </ul>
-            </div>
-        </div>
-</div>  
+  <div class="workCard">
+    <div class="heading">
+      <img
+        src={logo}
+        alt="company"
+        style={backgroundColor == "transparent"
+          ? "background-color: transparent;"
+          : `background-color: ${backgroundColor}; border-radius: 5px; padding: 5px;`}
+      />
+      <h1 class="title">{company}&nbsp;</h1>
+      <h1 class="title">{innerWidth > 786 ? "-" : ""} {title}</h1>
+      <h4 class="date">{date}</h4>
+    </div>
+    <span></span>
+    <div class="techStack">
+      {#each techStack as tech}
+        <p>{tech}</p>
+      {/each}
+    </div>
+    <span></span>
+    <div class="description">
+      <ul>
+        {#each description as item}
+          <li>{item}</li>
+        {/each}
+      </ul>
+    </div>
+  </div>
+</div>
 
 <style>
-    /* MOBILE */
-    @media screen and (max-width: 786px) {
-        .workCardWrapper {
-            width: 95%;
-        }
-
-        .workCardWrapper .workCard {
-            display: flex;
-            min-width: 100%;
-            flex-direction: column;
-            justify-content: center;
-            align-items: stretch;
-            margin: 0;
-            margin-top: 20px;
-            background-color: var(--indigo);
-            padding: 20px;
-            border-radius: 10px;
-        }
-
-        .workCardWrapper .workCard .heading {
-            display: flex;
-            flex-direction: column;
-            width: 100%;
-            justify-content: center;
-            align-items: center;
-            align-self: center;
-        }
-        
-        .workCardWrapper .workCard .heading img {
-            width: 50px;
-            border-radius: 50%;
-            margin-right: 10px;
-        }
-
-        .workCardWrapper .workCard .heading .company {
-            font-size: 32px;
-        }
-
-        .workCardWrapper .workCard .heading .title {
-            font-size: 20px;
-            font-weight: bold;
-            font-family: "Nunito";
-        }
-        
-        .workCardWrapper .workCard .date {
-            font-size: 18px;
-            font-weight: bold;
-            font-family: "Nunito";
-            width: 100%;
-            align-self: center;
-            text-align: center;
-        }
-
-        .workCardWrapper .workCard span {
-            border-bottom: 2px solid white;
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            width: 100%;
-            margin: 10px 0 ;
-        }
-
-        .workCardWrapper .workCard .techStack {
-            display: grid;
-            grid-template-columns: repeat(auto-fit, minmax(100px, 1fr) );
-            justify-content: space-between;
-            justify-items: center;
-            align-items: center;
-            width: 100%;
-        }
-    
-        .workCardWrapper .workCard .description {
-            width: 100%;
-            gap: 5px;
-        }
-
-        .workCardWrapper .workCard .description ul {
-            padding-left: 15px;
-
-        }
+  /* MOBILE */
+  @media screen and (max-width: 786px) {
+    .workCardWrapper {
+      width: 95%;
     }
 
-    @media screen and (min-width: 786px) {
-        .workCardWrapper {
-            width: 80%;
-            display: flex;
-            flex-direction: column;
-            justify-content: center;
-            align-items: center;
-        }
-
-        .workCardWrapper .workCard {
-            display: flex;
-            min-width: 100%;
-            flex-direction: column;
-            justify-content: center;
-            align-items: stretch;
-            margin: 20px;
-            background-color: var(--indigo);
-            padding: 20px;
-            border-radius: 10px;
-        }
-
-        
-        .workCardWrapper .workCard .heading {
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            align-self: center;
-        }
-        
-        .workCardWrapper .workCard .heading img {
-            height: 50px;
-            border-radius: 5px;
-            margin-right: 10px;
-        }
-        .workCardWrapper .workCard .heading .company {
-            font-size: 36px;
-        }
-
-        .workCardWrapper .workCard .heading .title {
-            font-size: 26px;
-            font-weight: bold;
-            font-family: "Nunito";
-        }
-
-        .workCardWrapper .workCard .date {
-            width: 100%;
-            text-align: center;
-            align-self: center;
-        }
-
-        .workCardWrapper .workCard span {
-            border-bottom: 2px solid white;
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            width: 100%;
-            margin: 10px 0 ;
-        }
-
-        .workCardWrapper .workCard .techStack {
-            display: flex;
-            justify-content: space-evenly;
-            align-items: center;
-            width: 100%;
-        }
-
-        .workCardWrapper .workCard .description {
-            width: 100%;
-            gap: 5px;
-        }
-
-        .workCardWrapper .workCard .description ul {
-            padding-left: 15px;
-            display: flex;
-            flex-direction: column;
-            justify-content: center;
-            align-items: flex-start;
-            gap: 8px;
-        }
+    .workCardWrapper .workCard {
+      display: flex;
+      min-width: 100%;
+      flex-direction: column;
+      justify-content: center;
+      align-items: stretch;
+      margin: 0;
+      margin-top: 20px;
+      background-color: var(--indigo);
+      padding: 20px;
+      border-radius: 10px;
     }
+
+    .workCardWrapper .workCard .heading {
+      display: flex;
+      flex-direction: column;
+      width: 100%;
+      justify-content: start;
+      align-items: center;
+      align-self: center;
+    }
+
+    .workCardWrapper .workCard .heading img {
+      width: 50px;
+      border-radius: 50%;
+      margin-right: 10px;
+    }
+
+    .workCardWrapper .workCard .heading .company {
+      font-size: 32px;
+    }
+
+    .workCardWrapper .workCard .heading .title {
+      font-size: 20px;
+      font-weight: bold;
+      font-family: "Nunito";
+    }
+
+    .workCardWrapper .workCard .date {
+      font-size: 18px;
+      font-weight: bold;
+      font-family: "Nunito";
+      width: 100%;
+      justify-content: start;
+      align-self: center;
+      text-align: center;
+    }
+
+    .workCardWrapper .workCard span {
+      border-bottom: 2px solid white;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      width: 100%;
+      margin: 10px 0;
+    }
+
+    .workCardWrapper .workCard .techStack {
+      display: grid;
+      grid-template-columns: repeat(auto-fit, minmax(100px, 1fr));
+      justify-content: space-between;
+      justify-items: center;
+      align-items: center;
+      width: 100%;
+    }
+
+    .workCardWrapper .workCard .description {
+      width: 100%;
+      gap: 5px;
+    }
+
+    .workCardWrapper .workCard .description ul {
+      padding-left: 15px;
+    }
+  }
+
+  @media screen and (min-width: 786px) {
+    .workCardWrapper {
+      width: 80%;
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+    }
+
+    .workCardWrapper .workCard {
+      display: flex;
+      min-width: 100%;
+      flex-direction: column;
+      justify-content: center;
+      align-items: stretch;
+      margin: 20px;
+      background-color: var(--indigo);
+      padding: 20px;
+      border-radius: 10px;
+    }
+
+    .workCardWrapper .workCard .heading {
+      display: flex;
+      width: 100%;
+      justify-content: space-between;
+      align-items: center;
+      align-self: center;
+    }
+
+    .workCardWrapper .workCard .heading img {
+      height: 50px;
+      border-radius: 5px;
+      margin-right: 10px;
+    }
+
+    .workCardWrapper .workCard .heading .title {
+      display: flex;
+      width: auto;
+      align-items: center;
+      justify-content: space-between;
+      font-size: 26px;
+      font-weight: bold;
+      font-family: "Nunito";
+      white-space: nowrap;
+    }
+
+    .workCardWrapper .workCard .date {
+      width: 100%;
+      text-align: end;
+      align-self: center;
+    }
+
+    .workCardWrapper .workCard span {
+      border-bottom: 2px solid white;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      width: 100%;
+      margin: 10px 0;
+    }
+
+    .workCardWrapper .workCard .techStack {
+      display: flex;
+      justify-content: space-evenly;
+      align-items: center;
+      width: 100%;
+    }
+
+    .workCardWrapper .workCard .description {
+      width: 100%;
+      gap: 5px;
+    }
+
+    .workCardWrapper .workCard .description ul {
+      padding-left: 15px;
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: flex-start;
+      gap: 8px;
+    }
+  }
 </style>
